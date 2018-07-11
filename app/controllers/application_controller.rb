3ca4a2833d5f857b7200c69439c929060319be4f
jebediahelliott <jebediahelliott@gmail.com>
2018-07-11 19:36:05 -0400

    Set up dotenv gem, update to ActiveRecord v 4.2.7.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   set :public_folder, 'public'
   set :views, 'app/views'
   enable :sessions
-  set :session_secret, "password_security"
+  set :session_secret, ENV.fetch('SESSION_SECRET')
   register Sinatra::Flash
 
 
